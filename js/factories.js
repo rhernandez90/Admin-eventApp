eef70728cf24d9f2827ea007266c1b90226230d6
Roberto Hernandez <rhernandezmunguia@gmail.com>
2017-10-19 21:33:01 -0600

    init projecct

---
+++ js/factories.js
@@ -0,0 +1,94 @@
+
+
+
+function loginFactory($http,$q,loginService){
+    'use strict';
+    //let token = loginService.getUserData().token || '';
+    let url   = loginService.getGlobalInfo().url;
+    var model = $q.defer();
+    model.logIn = function(data){
+        var request = $http({
+            method: "POST",
+            url: `${url}/auth`,
+            data:data,
+        });
+
+        return request;
+    };
+
+
+
+    return model;
+}
+
+function usersFactory($http,$q,loginService){
+    'use strict';
+    //let token = loginService.getUserData().token;
+    let url   = loginService.getGlobalInfo().url;
+    let route = "usuarios";
+    var model = $q.defer();
+
+
+    model.get = function(data = {}){
+        var request = $http({
+            method: "GET",
+            url: `${url}/${route}`,
+            params:data,
+        });
+        return request;
+    };
+
+    model.getOne = function(id){
+        var request = $http({
+            method: "GET",
+            url: `${url}/${route}/${id}`
+        });
+        return request;
+    };
+
+    model.create = function(data = {}){
+        var request = $http({
+            method: "POST",
+            url: `${url}/${route}`,
+            data:data,
+            headers: {'Content-Type': undefined}
+        });
+        return request;
+    };
+
+    model.delete = function(id = ""){
+        var request = $http({
+            method: "DELETE",
+            url: `${url}/${route}/${id}`
+        });
+        return request;
+    };
+
+    model.update = function(data={}){
+        var request = $http({
+            method: "PUT",
+            url: `${url}/${route}/${data.id}`,
+            data:data
+        });
+        return request;
+    };
+
+    model.resetPass = function(email=''){
+        var request = $http({
+            method: "GET",
+            url: `${url}/${route}/reset-pass`,
+            params:{email:email},
+        });
+        return request;
+    };
+
+    return model;
+}
+
+
+
+
+angular
+    .module('inspinia')
+    .factory('loginFactory', loginFactory)
+    .factory('usersFactory', usersFactory)
---
+++ js/services.js
@@ -0,0 +1,26 @@
+function loginService(){
+    
+        /*this.setUserData = (data)=>{
+            $sessionStorage.userData = data;
+        }
+    
+        this.getUserData = ()=>$sessionStorage.userData || {} ;
+    
+        this.logOut = ()=>{
+            delete $sessionStorage.userData;
+            $state.go('logins')
+        }
+        */
+        this.getGlobalInfo = ()=>{
+            return {
+                url:'http://192.168.0.130/webapi/api'
+                //url:'http://138.197.107.172:3000'
+            }
+        }
+    
+}
+
+
+angular
+.module('inspinia')
+.service('loginService', loginService)
